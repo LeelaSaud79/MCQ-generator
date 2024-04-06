a80fd9cbedf27505e32e08f270921d1b7a3fbcc6
bibek <bibek.dhakal.np@gmail.com>
2024-04-06 21:44:13 +0545

    changes in database

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from sense2vec import Sense2Vec
 import os
-from models.mcq import post_mca_questions
+#from models.mcq import post_mca_questions
 import csv
 from flask import Flask, request, jsonify, render_template, session, redirect
 import sqlite3
@@ -74,14 +74,14 @@ def submit():
             if count_words(text_input) > 100:
                 return jsonify({"error": "Word limit exceeded. Maximum 100 words allowed."}), 400
 
-            final_questions = post_mca_questions(text_input, s2v, num_questions=10)
-            # final_questions = [
-            # 'What plays a pivotal role in shaping the trajectory of human development?(a)education(b)navigate life(c)confidence necessary(d)educationCorrect answer is : (d)',
-            # 'What does Eric liu believe education is intrinsically linked to?(a)education(b)navigate life(c)navigate life(d)education significantly impacts healthCorrect answer is : (c)',
-            # 'What does Eric liu believe is the most important factor in human development?(a)education(b)navigate life(c)confidence necessary(d)education significantly impacts healthCorrect answer is : (c)',
-            # 'How does Eric liu feel about health and well being?(a)education(b)navigate life(c)education significantly impacts health(d)education significantly impacts healthCorrect answer is : (c)',
-            # 'What does Eric liu believe education does?(a)education(b)empower individuals(c)confidence necessary(d)education significantly impacts healthCorrect answer is : (b)'
-            #     ]
+            #final_questions = post_mca_questions(text_input, s2v, num_questions=10)
+            final_questions = [
+            'What plays a pivotal role in shaping the trajectory of human development?(a)education(b)navigate life(c)confidence necessary(d)educationCorrect answer is : (d)',
+            'What does Eric liu believe education is intrinsically linked to?(a)education(b)navigate life(c)navigate life(d)education significantly impacts healthCorrect answer is : (c)',
+            'What does Eric liu believe is the most important factor in human development?(a)education(b)navigate life(c)confidence necessary(d)education significantly impacts healthCorrect answer is : (c)',
+            'How does Eric liu feel about health and well being?(a)education(b)navigate life(c)education significantly impacts health(d)education significantly impacts healthCorrect answer is : (c)',
+            'What does Eric liu believe education does?(a)education(b)empower individuals(c)confidence necessary(d)education significantly impacts healthCorrect answer is : (b)'
+                ]
             print("Generated MCQs:", final_questions)
 
             # Store final_questions in session
