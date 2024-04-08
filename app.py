55edeb40789314c9aa8285c1a79130ee66964889
bibek <bibek.dhakal.np@gmail.com>
2024-04-08 08:07:16 +0545

    score calculation system

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from sense2vec import Sense2Vec
 import os
 from flask_limiter import Limiter
-from models.mcq import post_mca_questions
+#from models.mcq import post_mca_questions
 import csv
 from flask import Flask, request, jsonify, render_template, session, redirect
 import sqlite3
 from utils.db_helper import UserDatabase
-db =  UserDatabase(host="localhost", user="root", password="", database="data")
+db =  UserDatabase(host="localhost", user="root", password="Sim@nta2023", database="MCQ")
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'  # Set a secret key for session management
@@ -100,14 +100,14 @@ def submit():
             if count_words(text_input) > 500 and count_words(text_input) < 50:
                 return jsonify({"error": "Word limit exceeded. Maximum 500 words allowed."}), 400
 
-            final_questions = post_mca_questions(text_input, s2v, num_questions=10)
-            # final_questions = [
-            # 'What plays a pivotal role in shaping the trajectory of human development?(a)education(b)navigate life(c)confidence necessary(d)educationCorrect answer is : (d)',
-            # 'What does Eric liu believe education is intrinsically linked to?(a)education(b)navigate life(c)navigate life(d)education significantly impacts healthCorrect answer is : (c)',
-            # 'What does Eric liu believe is the most important factor in human development?(a)education(b)navigate life(c)confidence necessary(d)education significantly impacts healthCorrect answer is : (c)',
-            # 'How does Eric liu feel about health and well being?(a)education(b)navigate life(c)education significantly impacts health(d)education significantly impacts healthCorrect answer is : (c)',
-            # 'What does Eric liu believe education does?(a)education(b)empower individuals(c)confidence necessary(d)education significantly impacts healthCorrect answer is : (b)'
-            #     ]
+            #final_questions = post_mca_questions(text_input, s2v, num_questions=10)
+            final_questions = [
+            'What plays a pivotal role in shaping the trajectory of human development?(a)education(b)navigate life(c)confidence necessary(d)educationCorrect answer is : (d)',
+            'What does Eric liu believe education is intrinsically linked to?(a)education(b)navigate life(c)navigate life(d)education significantly impacts healthCorrect answer is : (c)',
+            'What does Eric liu believe is the most important factor in human development?(a)education(b)navigate life(c)confidence necessary(d)education significantly impacts healthCorrect answer is : (c)',
+            'How does Eric liu feel about health and well being?(a)education(b)navigate life(c)education significantly impacts health(d)education significantly impacts healthCorrect answer is : (c)',
+            'What does Eric liu believe education does?(a)education(b)empower individuals(c)confidence necessary(d)education significantly impacts healthCorrect answer is : (b)'
+                ]
             print("Generated MCQs:", final_questions)
 
             # Store final_questions in session
@@ -135,21 +135,25 @@ def submit():
 @app.route("/view_answer", methods=["POST"])
 def view_answer():
     correct_answers = []
+    count = 0 
+    correct_count= 0
     for i, (question, options, correct_answer) in enumerate(parsed_questions, start=0):
         selected_option = request.form.get(f"question_{i}")
         if selected_option is not None:
             selected_answer = selected_option.split(')')[0].strip()  # Extract and strip the selected option
             if selected_answer.lower() in correct_answer.lower():
                 result = "Correct"
+                correct_count += 1
             else:
                 result = "Incorrect"
         else:
             selected_answer = "No option selected"
             result = "Incorrect"
+        count +=1 
 
         correct_answers.append((question, options, selected_answer, correct_answer, result))
-
-    return render_template("view_answer.html", zipped_data=correct_answers)
+    percentage = correct_count/count*100 
+    return render_template("view_answer.html", zipped_data=correct_answers, percentage=percentage)
 
 
 if __name__ == '__main__':
